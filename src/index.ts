39df3f5c4d6acd6fa9392c365cc7cb7e06932df6
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-11 22:46:40 +0200

    more init

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import schema from "./schema";
 
-const server = new ApolloServer({ schema });
+async function main() {
+  const server = new ApolloServer({ schema });
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  console.log(`🚀  Server ready at: ${url}`);
+}
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
-
-console.log(`🚀  Server ready at: ${url}`);
+main().catch(console.error);
