575610a6fcd9668f70e990622993196d553900b9
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-16 22:29:49 +0200

    feat(definitions/helpers): improve session utils using jwt utils

--- src/definitions/helpers/Session.ts
+++ src/definitions/helpers/Session.ts
@@ -1,9 +1,8 @@
 import type { IdType } from '@sequelize-graphql/core';
 
-import * as jwt from 'jsonwebtoken';
 import { differenceInSeconds } from 'date-fns';
+import { signJWT, verifyJWT } from '@utils/jwt';
 import { Session } from '@definitions/models';
-import { JWT_SECRET_KEY } from '@constants/env';
 
 export async function createSession(userId: IdType, opts: { now: Date; expireAt: Date }) {
   const { now, expireAt } = opts;
@@ -13,19 +12,18 @@ export async function createSession(userId: IdType, opts: { now: Date; expireAt:
     expireAt,
     userId,
   });
-  const token = jwt.sign({ sessionId: session.id }, JWT_SECRET_KEY, { expiresIn: '5m' });
+  const token = signJWT({ sessionId: session.id }, { expiresIn: '5m' });
   const expiresIn = differenceInSeconds(expireAt, now);
-  const refreshToken = jwt.sign({ sessionId: session.id }, JWT_SECRET_KEY, { expiresIn });
+  const refreshToken = signJWT({ sessionId: session.id }, { expiresIn });
   return session.update({ token, refreshToken });
 }
 
 export function makeEmailVerificationToken(userId: IdType) {
-  return jwt.sign({ userId, type: 'emailVerification' }, JWT_SECRET_KEY, { expiresIn: '10m' });
+  return signJWT({ userId, type: 'emailVerification' }, { expiresIn: '10m' });
 }
 
 export function ensureEmailVerificationTokenPayload(token: string) {
-  const payload = jwt.verify(token, JWT_SECRET_KEY);
-  if (typeof payload === 'string') throw new Error('Invalid payload type');
+  const payload = verifyJWT(token);
   const { userId, type } = payload;
   if (!userId || type !== 'emailVerification') throw new Error('Invalid verification payload');
   return userId as IdType;
