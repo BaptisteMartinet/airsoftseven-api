bc3f638e49ef562ccb90791a1a76b3973a40b0e5
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-16 23:52:40 +0200

    feat(schema/query): implement session query

--- src/schema/query/index.ts
+++ src/schema/query/index.ts
@@ -1,13 +1,19 @@
-import { GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLObjectType } from 'graphql';
+import { Session } from '@definitions/models';
+import { ensureSessionUser } from '@/definitions/helpers/Session';
 
 export default new GraphQLObjectType({
   name: 'Query',
-  fields: {
-    helloWorld: {
-      type: GraphQLString,
-      resolve() {
-        return 'Hello World';
+  fields: () => ({
+    session: {
+      type: Session.type,
+      async resolve(_, args, ctx) {
+        try {
+          return ensureSessionUser(ctx);
+        } catch (e) {
+          return null;
+        }
       },
     },
-  },
+  }),
 });
