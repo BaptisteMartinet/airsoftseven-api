055f1d3eee5befd8dd96d4b5867ef5397eaedcd1
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-07-27 17:46:53 +0200

    feat(definitions/models): implement EmailVerificationCode model

---
+++ src/definitions/models/EmailVerificationCode.ts
@@ -0,0 +1,23 @@
+import type { InferModelAttributesWithDefaults } from '@sequelize-graphql/core';
+
+import { Model, STRING, DATE } from '@sequelize-graphql/core';
+import sequelize from '@db/index';
+
+export interface EmailVerificationCodeModel extends InferModelAttributesWithDefaults<EmailVerificationCodeModel> {
+  email: string;
+  code: string;
+  expireAt: Date;
+}
+
+const EmailVerificationCode: Model<EmailVerificationCodeModel> = new Model({
+  name: 'EmailVerificationCode',
+  columns: {
+    email: { type: STRING, allowNull: false, exposed: false },
+    code: { type: STRING, allowNull: false, exposed: false },
+    expireAt: { type: DATE, allowNull: false, exposed: false },
+  },
+  indexes: [{ fields: ['email'], unique: true }],
+  sequelize,
+});
+
+export default EmailVerificationCode;
--- src/definitions/models/index.ts
+++ src/definitions/models/index.ts
@@ -3,3 +3,4 @@ export { default as Session, type SessionModel } from './Session';
 export { default as Club, type ClubModel } from './Club';
 export { default as Field, type FieldModel } from './Field';
 export { default as Event, type EventModel } from './Event';
+export { default as EmailVerificationCode, type EmailVerificationCodeModel } from './EmailVerificationCode';
