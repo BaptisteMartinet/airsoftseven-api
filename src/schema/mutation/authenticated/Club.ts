b1c0bd5b96f4d2e3aff58f2003a343938c484d97
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-08-04 18:47:50 +0200

    feat(schema/mutation): implement more fields on Club mutation

--- src/schema/mutation/authenticated/Club.ts
+++ src/schema/mutation/authenticated/Club.ts
@@ -1,4 +1,4 @@
-import { GraphQLNonNull, GraphQLString } from 'graphql';
+import { GraphQLBoolean, GraphQLNonNull, GraphQLString } from 'graphql';
 import { genModelMutations } from '@sequelize-graphql/core';
 import { Club } from '@definitions/models';
 import { ensureSessionUser } from '@definitions/helpers/Session';
@@ -8,11 +8,16 @@ export default genModelMutations(Club, {
   create: {
     args: {
       name: { type: new GraphQLNonNull(GraphQLString) },
+      description: { type: GraphQLString },
+      publicUrl: { type: GraphQLString },
+      rules: { type: GraphQLString },
+      rental: { type: GraphQLBoolean },
+      acceptUnderage: { type: GraphQLBoolean },
     },
     async resolve(_, args, ctx) {
-      const { name } = args;
+      const fields = args;
       const user = await ensureSessionUser(ctx);
-      return Club.model.create({ name, userId: user.id });
+      return Club.model.create({ ...fields, userId: user.id });
     },
   },
 });
