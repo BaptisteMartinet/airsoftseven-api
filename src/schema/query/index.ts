d3648dc870906c5838480b7f5da67934662bafef
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-09-11 22:31:32 +0200

    feat(schema/query): implement find by slug endpoints

--- src/schema/query/index.ts
+++ src/schema/query/index.ts
@@ -1,9 +1,26 @@
+import type { Model } from '@sequelize-graphql/core';
 import type { Context } from '@/context';
 
-import { GraphQLObjectType } from 'graphql';
+import { GraphQLFieldConfig, GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
 import { exposeModel } from '@sequelize-graphql/core';
+import { ResourceDoesNotExist } from '@/utils/errors';
 import { Session, Event, Club, Field } from '@definitions/models';
 import { ensureSession } from '@/definitions/helpers/Session';
+
+function makeFindBySlugField(model: Model<any>) {
+  return {
+    type: new GraphQLNonNull(model.type),
+    args: {
+      slug: { type: new GraphQLNonNull(GraphQLString) },
+    },
+    async resolve(_, args, ctx) {
+      const { slug } = args;
+      const instance = await model.model.findOne({ where: { slug } });
+      if (!instance) throw new ResourceDoesNotExist(Event.name, slug);
+      return instance;
+    },
+  } as const satisfies GraphQLFieldConfig<unknown, Context>;
+}
 
 export default new GraphQLObjectType<unknown, Context>({
   name: 'Query',
@@ -20,21 +37,27 @@ export default new GraphQLObjectType<unknown, Context>({
     },
 
     ...exposeModel(Event, {
-      findById: 'event',
+      findById: false,
       findByIds: false,
       pagination: 'events',
     }),
 
+    event: makeFindBySlugField(Event),
+
     ...exposeModel(Club, {
-      findById: 'club',
+      findById: false,
       findByIds: false,
       pagination: 'clubs',
     }),
 
+    club: makeFindBySlugField(Club),
+
     ...exposeModel(Field, {
-      findById: 'field',
+      findById: false,
       findByIds: false,
       pagination: 'fields',
     }),
+
+    field: makeFindBySlugField(Field),
   }),
 });
