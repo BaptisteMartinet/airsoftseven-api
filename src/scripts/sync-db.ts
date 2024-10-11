1945fb35e328af5948b86ce012ee6f7f2e512c00
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-10-11 23:59:20 +0200

    feat(scripts): implement sync-db force and alter args

--- src/scripts/sync-db.ts
+++ src/scripts/sync-db.ts
@@ -2,13 +2,21 @@ import type { Model } from '@sequelize-graphql/core';
 import db from '@db/index';
 import * as RawModels from '@definitions/models';
 
+function parseArgs(args: Array<string>) {
+  const [node, path, ...restArgs] = args;
+  return new Set(restArgs);
+}
+
 // A very simple syncModels implementation
 async function syncModels(models: Array<Model<any>>) {
+  const args = parseArgs(process.argv);
   await db.authenticate();
   console.info('DB authenticated');
+  const force = args.has('--force');
+  const alter = args.has('--alter');
   for (const model of models) {
     model.associations;
-    await model.model.sync();
+    await model.model.sync({ force, alter });
   }
   console.info(`Synced ${models.length} models.`);
 }
