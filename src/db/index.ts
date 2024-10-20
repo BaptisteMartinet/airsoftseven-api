be8b5d26d5ecc2b201ee2b6bcfa42c83610672ac
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-10-20 19:23:02 +0200

    feat: implement disable db logging

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
 import { Sequelize } from 'sequelize';
-import { __DATABASE_URL__ } from '@constants/env';
+import { __DATABASE_URL__, __DISABLE_DB_LOGGING__ } from '@constants/env';
 
-export default new Sequelize(__DATABASE_URL__);
+export default new Sequelize(__DATABASE_URL__, { logging: !__DISABLE_DB_LOGGING__ });
