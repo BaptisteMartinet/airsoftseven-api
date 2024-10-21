09c572609a1f7dc485c0f4140474270d019a29eb
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-10-21 22:37:57 +0200

    [API] feat(event-schema): implement Event.interest field

--- src/schema/model/Event.ts
+++ src/schema/model/Event.ts
@@ -1,6 +1,6 @@
 import { GraphQLBoolean, type GraphQLFieldConfigMap } from 'graphql';
 import type { Context } from '@/context';
-import { EventReport, type EventModel } from '@/definitions/models';
+import { EventInterest, EventReport, type EventModel } from '@/definitions/models';
 import { getSessionUser } from '@/definitions/helpers/Session';
 
 export default function makeEventFields() {
@@ -11,6 +11,14 @@ export default function makeEventFields() {
         const user = await getSessionUser(ctx);
         if (!user) return false;
         return EventReport.exists({ where: { authorId: user.id, eventId: event.id } });
+      },
+    },
+    interested: {
+      type: GraphQLBoolean,
+      async resolve(event, args, ctx) {
+        const user = await getSessionUser(ctx);
+        if (!user) return false;
+        return EventInterest.exists({ where: { userId: user.id, eventId: event.id } });
       },
     },
   } as const satisfies GraphQLFieldConfigMap<EventModel, Context>;
