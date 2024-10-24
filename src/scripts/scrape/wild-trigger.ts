a7e3cc474034eeb6d3e3bcfc53b927812053ba81
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-10-24 20:41:24 +0200

    feat: improve wild trigger scraping script

--- src/scripts/scrape/wild-trigger.ts
+++ src/scripts/scrape/wild-trigger.ts
@@ -12,18 +12,19 @@ import { runWithDBAndHandleErrors } from '@scripts/common';
 
 const WildTriggerFieldsBaseURL = 'https://wildtrigger.com/categorie-produit/terrain';
 const SleepDelayBetweenPagesMs = 1 * Second;
+const SleepDelayBetweenDetailsPagesMs = 1 * Second;
 const BaptisteId = '120e6d2e-dae5-4cdd-b68d-4a0f2c90f1d8'; // TODO change
 const WildTriggerClubId = '96e4eed5-37a4-4f29-85d1-e34277a50ca8'; // TODO change
 
 // TODO update values
 const FieldsInfoPerFieldName = new Map<string, { id: IdType; capacity: number }>([
   ['agency', { id: '6526deb3-34bb-4052-ae8e-711c7b348a0e', capacity: 60 }],
-  ['nordisk', { id: '0d015900-38f5-4728-9de3-d26dbbfcafdb', capacity: 60 }],
-  ['territory', { id: '9a9c26bf-821c-41ca-8a03-6cadc8b34c5c', capacity: 60 }],
-  ['new agency', { id: 'c4d56819-79a2-4d86-ac5a-c68cc2270e97', capacity: 60 }],
-  ['domain', { id: 'ca40c1da-0357-4f89-abc4-59ab9dad5f87', capacity: 60 }],
-  ['mosaïc', { id: '9ebb747f-6b65-450a-9378-520a1f51c80c', capacity: 60 }],
-  ['complex', { id: '9b41b315-d9c5-42ed-b1f4-fbc58fc019b6', capacity: 60 }],
+  ['nordisk', { id: '0d015900-38f5-4728-9de3-d26dbbfcafdb', capacity: 40 }],
+  ['territory', { id: '9a9c26bf-821c-41ca-8a03-6cadc8b34c5c', capacity: 24 }],
+  ['new agency', { id: 'c4d56819-79a2-4d86-ac5a-c68cc2270e97', capacity: 240 }],
+  ['domain', { id: 'ca40c1da-0357-4f89-abc4-59ab9dad5f87', capacity: 150 }],
+  ['mosaïc', { id: '9ebb747f-6b65-450a-9378-520a1f51c80c', capacity: 50 }],
+  ['complex', { id: '9b41b315-d9c5-42ed-b1f4-fbc58fc019b6', capacity: 150 }],
   ['virgo', { id: 'babde254-a306-4ea2-8014-59a93687dac6', capacity: 60 }],
 ]);
 
@@ -31,57 +32,14 @@ function makeEventsPageURL(pageNum: number) {
   return WildTriggerFieldsBaseURL + `/page/${pageNum}`;
 }
 
-type EventT = Awaited<ReturnType<typeof scrapePage>>[number];
-
-async function scrapePage(url: string) {
-  const page = await cheerio.fromURL(url);
-  const data = page.extract({
-    products: [
-      {
-        selector: '.products li',
-        value: {
-          publicURL: {
-            selector: 'a',
-            value: 'href',
-          },
-          date: {
-            selector: '.event-date',
-          },
-          title: {
-            selector: '.woocommerce-loop-product__title',
-          },
-          fieldName: {
-            selector: '.ast-woo-product-category',
-          },
-          price: {
-            selector: 'bdi',
-          },
-        },
-      },
-    ],
-  });
-  return data.products;
-}
-
-async function scrapePages() {
-  const events: Array<EventT> = [];
-  let pageNum = 1;
-  let running = true;
-
-  while (running) {
-    const url = makeEventsPageURL(pageNum);
-    try {
-      const pageEvents = await scrapePage(url);
-      events.push(...pageEvents);
-      console.info(`Scraped ${pageEvents.length} events on page ${pageNum}. ${events.length} in total.`);
-      await sleep(SleepDelayBetweenPagesMs);
-      pageNum += 1;
-    } catch (err) {
-      running = false;
-      console.info(`Stop reason: ${err}`);
-    }
-  }
-  return events;
+interface ParsedEvent {
+  title: string;
+  date: Date;
+  durationDays: number;
+  price: number;
+  publicURL: string;
+  // Details
+  fieldName: string;
 }
 
 function parseRawEventFieldName(rawFielName: string) {
@@ -121,35 +79,97 @@ function parseRawEventPrice(rawPrice: string) {
   return price;
 }
 
-function parseRawEvent(event: EventT) {
-  const { fieldName: rawFieldName, title, date: rawDate, price: rawPrice, publicURL } = event;
-  assert(rawFieldName && title && rawDate && rawPrice && publicURL, `Invalid event data: ${JSON.stringify(event)}`);
-  const { date, durationDays } = parseRawEventDateAndDuration(rawDate);
-  const price = parseRawEventPrice(rawPrice);
-  const fieldName = parseRawEventFieldName(rawFieldName);
-  return {
-    title,
-    date,
-    durationDays,
-    price,
-    publicURL,
-    fieldName,
-  };
+async function scrapeEventPageDetails(url: string) {
+  const page = await cheerio.fromURL(url);
+  const data = page.extract({
+    fieldName: '.woocommerce-breadcrumb > a:nth-of-type(3)',
+  });
+  return data;
 }
 
-async function createEvents(rawEvents: Array<EventT>) {
+async function scrapeEventsPage(url: string) {
+  const page = await cheerio.fromURL(url);
+  const data = page.extract({
+    products: [
+      {
+        selector: '.products li',
+        value: {
+          publicURL: {
+            selector: 'a',
+            value: 'href',
+          },
+          date: {
+            selector: '.event-date',
+          },
+          title: {
+            selector: '.woocommerce-loop-product__title',
+          },
+          price: {
+            selector: 'bdi',
+          },
+        },
+      },
+    ],
+  });
+  const events: Array<ParsedEvent> = [];
+  for (const rawEvent of data.products) {
+    const { publicURL, date: rawDate, title, price: rawPrice } = rawEvent;
+    assert(title && rawDate && rawPrice && publicURL, `Invalid event data: ${JSON.stringify(rawEvent)}`);
+    const { date, durationDays } = parseRawEventDateAndDuration(rawDate);
+    const price = parseRawEventPrice(rawPrice);
+    const details = await scrapeEventPageDetails(publicURL);
+    const { fieldName: rawFieldName } = details;
+    if (!rawFieldName) {
+      console.info(`Ignoring event ${title} because details are not available: ${publicURL}`);
+      continue;
+    }
+    const fieldName = parseRawEventFieldName(rawFieldName);
+    events.push({
+      title,
+      date,
+      durationDays,
+      price,
+      publicURL,
+      // details
+      fieldName,
+    });
+    await sleep(SleepDelayBetweenDetailsPagesMs);
+  }
+  return events;
+}
+
+async function scrapeEventsPages() {
+  let events: Array<ParsedEvent> = [];
+  let pageNum = 1;
+  let running = true;
+
+  while (running) {
+    const url = makeEventsPageURL(pageNum);
+    try {
+      const pageEvents = await scrapeEventsPage(url);
+      events = events.concat(pageEvents);
+      console.info(`Scraped ${pageEvents.length} events on page ${pageNum}. ${events.length} in total.`);
+      await sleep(SleepDelayBetweenPagesMs);
+      pageNum += 1;
+    } catch (err) {
+      running = false;
+      console.info(`Stop reason: ${err}`);
+    }
+  }
+  return events;
+}
+
+async function createEvents(events: Array<ParsedEvent>) {
   const ctx = makeContext();
   const user = await User.ensureExistence(BaptisteId, { ctx });
   const club = await Club.ensureExistence(WildTriggerClubId, { ctx });
-  const now = new Date();
-  const dateTzOffset = now.getTimezoneOffset();
 
   const unknownFieldNames: Array<string> = [];
   let existingEventsCount = 0;
   let createdEventsCount = 0;
 
-  for (const rawEvent of rawEvents) {
-    const { title, date, durationDays, price, publicURL, fieldName } = parseRawEvent(rawEvent);
+  for (const event of events) {
+    const { fieldName, title, date, durationDays, price, publicURL } = event;
     const fieldInfo = FieldsInfoPerFieldName.get(fieldName);
     if (!fieldInfo) {
       unknownFieldNames.push(fieldName);
@@ -182,7 +202,7 @@ async function createEvents(rawEvents: Array<EventT>) {
       slugBase,
       title,
       date,
-      dateTzOffset,
+      dateTzOffset: date.getTimezoneOffset(),
       durationDays,
       price,
       capacity,
@@ -194,7 +214,7 @@ async function createEvents(rawEvents: Array<EventT>) {
     createdEventsCount += 1;
   }
   console.info(' ');
-  console.info(`${rawEvents.length} found.`);
+  console.info(`${events.length} found.`);
   console.info(`${createdEventsCount} created.`);
   console.info(`${existingEventsCount + unknownFieldNames.length} ignored.`);
   console.info(`- ${existingEventsCount} already existing.`);
@@ -202,7 +222,7 @@ async function createEvents(rawEvents: Array<EventT>) {
 }
 
 async function main() {
-  const events = await scrapePages();
+  const events = await scrapeEventsPages();
   await createEvents(events);
   console.info('Done');
 }
